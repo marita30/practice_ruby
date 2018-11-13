3a1be6162191832f88e763a1676eaf1070b1bb88
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-13 19:36:04 +0000

    Exercise cleaned

---
+++ chapter2/quiz1/questions/question_1.rb
@@ -0,0 +1,2 @@
+# Write a program that uses constants to define the numbers 1 to 10 as
+# numerals.
---
+++ chapter3/quiz1/questions/question_1.rb
@@ -0,0 +1,4 @@
+# Uncomment each line and fill in the blanks so that the arithmetic
+# operations produce the correct value for each variable.  e.g. in
+# `four = 2.+()`, the calculation to the right of the `=` should
+# evaluate to 4. So it would become `four = 2.+(2)`.
